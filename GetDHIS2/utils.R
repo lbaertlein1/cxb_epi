29979997366a780138d31f2fd5cb2567f94f5a39
Luke Baertlein <baertlein.luke@gmail.com>
2022-10-09 17:30:18 +0600

    added get all data elements feature if element group  not selected

--- GetDHIS2/utils.R
+++ GetDHIS2/utils.R
@@ -1,4 +1,4 @@
-pacman::p_load(yaml, tidyverse, jsonlite, purrr, data.table, devtools)
+pacman::p_load(yaml, tidyverse, jsonlite, purrr, data.table, devtools, utils)
 devtools::install_github(repo = "https://github.com/pepfar-datim/datimutils.git", ref = "master")
 
 #' Check to see if cache exists, if not create it
@@ -64,29 +64,45 @@ load_specs <- function(folder = Sys.getenv("dhis2_folder")){
     return(metadata)
   }
 
-getData <- function(data_element_group = "OPD - New Consultations",
-                    country = "Bangladesh",
-                    projects = c("Balukhali Healthcare", "Kutupalong Healthcare"),
-                    time_period_type = "MONTHS",
-                    start_date = "2020-01-01",
-                    end_date = Sys.Date(),
-                    metadata = metadata){
-  #convert time period to list
+#convert time period to list
   datseq <- function(t1, t2) { 
     format(seq(as.Date(t1, "%Y-%m-%d"), 
                as.Date(t2, "%Y-%m-%d"),by="month"), 
            "%Y%m") 
   }
+  
+getData <- function(data_element_group = "OPD - New Consultations",
+                    country = "Bangladesh",
+                    projects = NULL,
+                    time_period_type = "MONTHS",
+                    start_date = "2020-01-01",
+                    end_date = Sys.Date(),
+                    metadata = metadata,
+                    org_unit_hierarchy = 5,
+                    relative_time_period = "LAST_12_MONTHS"){
     if(time_period_type == "MONTHS"){
       time_period <- datseq(t1 = start_date,
                              t2 = end_date)
     }
+    if(time_period_type == "RELATIVE"){
+      time_period <- relative_time_period
+    }
   
+  if(!is.null(data_element_group)){
   data_elements <-  rbindlist(map(metadata[["dataElementGroups"]], as.data.table), fill = TRUE, idcol = T) %>%
     filter(name == data_element_group) %>%
     select(dataElements) %>%
     unnest(cols=c(dataElements)) %>%
     unnest(cols=c(dataElements))
+  }
+  if(is.null(data_element_group)){
+    data_elements <-  rbindlist(map(metadata[["dataElements"]], as.data.table), fill = TRUE, idcol = T) %>%
+      filter(aggregationType == "SUM") %>%
+      filter(!(grepl(paste0(c("Geo-locations","EGEN_","GL_", "EVAC_", " location"), collapse="|"), name, ignore.case=TRUE))) %>%
+      select(id, name, aggregationType) %>%
+      unique() %>%
+      rename(dataElements = id)
+  }
 
   country_id <- rbindlist(map(metadata[["organisationUnits"]], as.data.table), fill = TRUE, idcol = T) %>%
     filter(name == country & level == "3") %>%
@@ -96,11 +112,27 @@ getData <- function(data_element_group = "OPD - New Consultations",
   project_list <-  rbindlist(map(metadata[["organisationUnits"]], as.data.table), fill = TRUE, idcol = T) %>%
     filter(level == "4" & grepl(country_id$id[1], path) & name != "_Geolocations") %>%
     select(name, id) %>%
-    unique() %>%
-    filter(name %in% projects)
+    unique() 
   
-  facility_list <- rbindlist(map(metadata[["organisationUnits"]], as.data.table), fill = TRUE, idcol = T) %>%
-    filter(as.numeric(level) == 5 & grepl(paste0(project_list$id, collapse="|"), path) & name != "_Geolocations") %>%
+  if(!is.null(projects)){
+    project_list <- project_list %>%
+      filter(name %in% projects)
+  }
+  if(is.null(projects)){
+    add_all <- project_list %>%
+      bind_rows(c(name = "Select All"))
+    selection <- add_all[utils::menu(add_all$name, title="Select a Project:")]
+    if(selection$name[1] == "Select All"){
+      project_list <- project_list
+    }
+    if(selection$name != "Select All"){
+      project_list <- project_list %>%
+        filter(name == selection$name[1])
+    }
+  }
+  
+  org_unit_list <- rbindlist(map(metadata[["organisationUnits"]], as.data.table), fill = TRUE, idcol = T) %>%
+    filter(as.numeric(level) == org_unit_hierarchy & grepl(paste0(project_list$id, collapse="|"), path) & name != "_Geolocations") %>%
     select(name, id) %>%
     unique() 
   
@@ -111,15 +143,29 @@ getData <- function(data_element_group = "OPD - New Consultations",
     username = load_specs()$dhis2$username,
     password = load_specs()$dhis2$password
   )
-    
-  data <- datimutils::getAnalytics(dx = dput(data_elements$dataElements),
-                                   ou = dput(facility_list$id),
-                                   pe = dput(time_period),
-                                  return_names = TRUE)
-  return(data)
+  
+  for(i in 1:length(data_elements$dataElements)){
+    print(i)
+    data <- datimutils::getAnalytics(dx = data_elements$dataElements[i],
+                                     ou = dput(org_unit_list$id),
+                                     pe = dput(time_period),
+                                     return_names = TRUE)
+    if(i == 1){
+      all_data <- data
+    }
+    if(i != 1){
+      all_data <- all_data %>%
+        bind_rows(data)
+    }
+  }
+  # data <- datimutils::getAnalytics(dx = dput(data_elements$dataElements),
+  #                                  ou = dput(org_unit_list$id),
+  #                                  pe = dput(time_period),
+  #                                 return_names = TRUE)
+  return(all_data)
 }
 
-#Possible periods: 
+#Possible relative periods: 
 # THIS_WEEK, LAST_WEEK, LAST_4_WEEKS, LAST_12_WEEKS, LAST_52_WEEKS,
 # THIS_MONTH, LAST_MONTH, THIS_BIMONTH, LAST_BIMONTH, THIS_QUARTER, LAST_QUARTER,
 # THIS_SIX_MONTH, LAST_SIX_MONTH, MONTHS_THIS_YEAR, QUARTERS_THIS_YEAR,

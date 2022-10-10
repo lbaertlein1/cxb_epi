a0488b266fb97c377d0d83ef27c36b0426637f53
Luke Baertlein <baertlein.luke@gmail.com>
2022-10-10 15:16:34 +0600

    built out getKOBO function to select and download kobo datasets

--- GetKOBO/utils.R
+++ GetKOBO/utils.R
@@ -1,4 +1,4 @@
-pacman::p_load(httr, readr, tidyverse)
+pacman::p_load(httr, readr, tidyverse, lubridate)
 
 init_kobo_data_struc <- function(folder, base_url, username, password){
   #check to see if folder exists, if not create it
@@ -41,4 +41,56 @@ load_specs <- function(folder = Sys.getenv("kobo_folder")){
   return(specs)
 }
 
+#Select asset and download
+
+getKOBO <- function(asset_id = NULL,
+                    get_all_cebs = FALSE,
+                    get_all = FALSE){
+  if(!is.null(asset_id)){
+    l <- kobo_asset_list() %>%
+      arrange(desc(as.Date(date_modified, origin=lubridate::origin))) %>%
+      select(uid, name) %>%
+      filter(!is.na(name) & name != "")
+    selection <- l %>% 
+      filter(uid %in% asset_id)
+    asset_id <- selection$uid
+    asset_name <- selection$name
+  }
+  if(is.null(asset_id) & get_all_cebs == FALSE & get_all == FALSE){
+    l <- kobo_asset_list() %>%
+      arrange(desc(as.Date(date_modified, origin=lubridate::origin))) %>%
+      select(uid, name) %>%
+      filter(!is.na(name) & name != "")
+    selection <- l %>% 
+      filter(name == l$name[utils::menu(l$name, title="Select an Asset:")])
+    asset_id <- selection$uid
+    asset_name <- selection$name
+  }
+  if(get_all_cebs == TRUE & get_all == FALSE){
+    asset_id <- c("acfGrxnmkTaFvSTdHnrQ3x", "a9bYeVttuVuSzCcgmk9qpo", "a4V5jXFEHjwALe8ztWQNvX", "a6GKjVvR7dyEPeXDtQ9U37", "a5BCfuooic3qN8Y27pR44M")
+    l <- kobo_asset_list() %>%
+      arrange(desc(as.Date(date_modified, origin=lubridate::origin))) %>%
+      select(uid, name) %>%
+      filter(!is.na(name) & name != "")
+    selection <- l %>% 
+      filter(uid %in% asset_id)
+    asset_id <- selection$uid
+    asset_name <- selection$name
+  }
+  if(get_all == TRUE){
+    selection <- kobo_asset_list() %>%
+      arrange(desc(as.Date(date_modified, origin=lubridate::origin))) %>%
+      select(uid, name) %>%
+      filter(!is.na(name) & name != "")
+    asset_id <- selection$uid
+    asset_name <- selection$name
+  }
+  output_list <- list()
+  for(i in 1:length(asset_id)){
+    data <- kobo_data(kobo_asset(asset_id[i]))
+    output_list <- append(output_list, list(new=data))
+    names(output_list)[i] <- asset_name[i]
+  }
+  return(output_list)
+}
 
